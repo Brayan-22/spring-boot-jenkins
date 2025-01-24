2bc831c88edd2802d4a8d151212fd0a12519c9f6
alejandro <alejandroriveros100@gmail.com>
2025-01-24 13:20:22 -0500

    [Fix] orderService

--- src/main/java/dev/alejandro/jenkinsproject/advices/OrderControllerAdivce.java
+++ src/main/java/dev/alejandro/jenkinsproject/advices/OrderControllerAdivce.java
@@ -2,20 +2,24 @@ package dev.alejandro.jenkinsproject.advices;
 
 import dev.alejandro.jenkinsproject.exceptions.OrderNotCreatedException;
 import dev.alejandro.jenkinsproject.exceptions.OrderNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class OrderControllerAdivce {
 
     @ExceptionHandler(OrderNotCreatedException.class)
     public ResponseEntity<?> handleOrderNotCreatedException(OrderNotCreatedException ex) {
+        log.error("Order not created", ex);
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
     @ExceptionHandler(OrderNotFoundException.class)
     public ResponseEntity<?> handleOrderNotFoundException(OrderNotFoundException ex) {
+        log.error("Order not found", ex);
         return ResponseEntity.notFound().build();
     }
 }
